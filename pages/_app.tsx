9fd49d91511687d2bb4a3361a06135f85beda2fd
AviroopNandy <aviroopnandy2001@gmail.com>
2022-06-04 11:19:20 +0530

    fix: persist state after page refresh

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,40 @@
 import type { AppProps } from "next/app";
+/* eslint-disable */
+// @ts-ignore
+import storage from "redux-persist/lib/storage";
+/* eslint-enable */
+import { combineReducers } from "redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistReducer, persistStore } from "redux-persist";
 import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
 import userReducer from "../features/User";
 
 import "../styles/globals.css";
 
-const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
+const reducers = combineReducers({
+  user: userReducer,
 });
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+const store = configureStore({
+  reducer: persistedReducer,
+});
+
+let persistor = persistStore(store);
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <Component {...pageProps} />;
+      <PersistGate loading={null} persistor={persistor}>
+        <Component {...pageProps} />;
+      </PersistGate>
     </Provider>
   );
 }
